724998f85f8e87cbc1a8d4147400e36a2dc4f477
Eunice Choi <euniceschoi93@gmail.com>
2015-02-28 06:17:33 +0000

    Project update on organizing webscraped pop trend data and creating bar graph

--- poptrend.py
+++ poptrend.py
@@ -15,3 +15,9 @@ wordcount = Counter(file.read().split())
 for item in wordcount.items(): print("{}\t{}".format(*item))
 
 file.close()
+
+#create bar graph in R
+poptrend=c(2,5,30)
+colors=c("blue", "green", "red")
+barplot(poptrend, main="Population Trend of Felidae", 
+        xlab="Population Trend", ylab="# Species",names.arg=c("Unknown","Stable","Decreasing"), col=colors)
